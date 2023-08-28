63767937469bca793e18f3eb6e55a38f51c1682b
myingineer <e.alexander190@yahoo.com>
2023-08-28 22:50:48 +0100

    version-2.0

--- script.js
+++ script.js
@@ -1,40 +1,88 @@
+// Getting the number at the top of the cart
 const numOnCart = document.querySelector('.num');
+
+// Getting the add btn
 const addBtn = document.querySelector('.addbtn');
+
+// Getting the subtract btn
 const subtractBtn = document.querySelector('.minusbtn');
+
+// Getting the add to cart btn
 const addToCart = document.querySelector('.add-to-cart');
+
+// Getting the number increase inside the cart
 const insideCartNumber = document.querySelector('.number');
+
+// Getting the increament display in between the add and minus btn
 const increaseDisplay = document.querySelector('.increament');
+
+// Getting the total amount inside the cart
 const totalAmount = document.querySelector('.total-amount');
+
+// Getting the delete button inside the cart
 const deleteBtn = document.querySelector('.delete');
+
+// Getting the content inside the cart
 const cartContent = document.querySelector('.content-in-cart');
+
+// Your cart is empty text
 const empty = document.querySelector('.empty');
+
+// Getting the main image on the landing page display
 const mainImage = document.querySelector('.main-image-display');
+
+// Getting all the images for the main page display
 const allImages = document.querySelectorAll('.small-image');
+
+// Getting the cart icon
 const cartBtn = document.querySelector('.dropdown-icon');
+
+// Cart container button
 const cart = document.querySelector('.dropdown-content');
+
+// Checkout button inside the cart
 const checkOutBtn = document.querySelector('.checkout');
-const lightbox = document.querySelector('.lightbox-container')
+
+// Light box container
+const lightbox = document.querySelector('.lightbox-container');
+
+// Close button inside the light box
 const closeBtn = document.querySelector('.close-btn');
+
+// Big display for the light box
 const bigLightbox = document.querySelector('.big-lightbox');
+
+// Small images under the light box
 const lightboxThumbnails = document.querySelectorAll('.small-thumbnails');
+
+// Getting the left and right button inside the lightbox
 const leftButton = document.querySelector('.left-button');
 const rightButton = document.querySelector('.right-button');
+
+// Getting the open menu button
 const openMenuButton = document.querySelector('.ham');
+
+// Getting the close menu button
 const closeMenuButton = document.querySelector('.close-menu');
 
+// open Menu for phone
 openMenuButton.addEventListener('click', () => {
     openMenu();
 });
 
+// Close menu for phone
 closeMenuButton.addEventListener('click', () => {
     closeMenu();
 });
 
+
+// Function to open the menu
 function openMenu() {
     let menu = document.querySelector('.mobile-menu');
     menu.classList.add('display');
 }
 
+// Function to close the menu
 function closeMenu() {
     let menu = document.querySelector('.mobile-menu');
     menu.classList.remove('display');
@@ -99,6 +147,7 @@ lightboxThumbnails.forEach(image => {
 let number = 0;
 let total = 0;
 
+// Resuseable function
 function finishedOrder() {
     number = 0;
     increaseDisplay.innerText = 0;
